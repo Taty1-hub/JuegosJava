a1ccad1e96e817f0ac26d7e4f30eda1e7fde8ae1
Sandra <docenciasandrag@gmail.com>
2020-07-29 17:29:54 +0200

    creadores de acceso y propiedades staticas o de clase

--- src/com/company/Juego.java
+++ src/com/company/Juego.java
@@ -2,20 +2,45 @@ package com.company;
 
 public class Juego {
 
-    public int vidas;
-    public int MuestraVidasRestantes;
-
-    public static void main(String[] args) {
-        // write your code here
-    }
+        private int vidas;
+        private int vidasIniciadas;
+        private static int record;
 
 
-    public Juego(int vidas, int muestraVidasRestantes) {
-        this.vidas = vidas;
-        MuestraVidasRestantes = muestraVidasRestantes;
-    }
+        public Juego(int vidas) {
+            this.vidas = vidas;
+            this.vidasIniciadas=vidas;
+        }
 
-    public Juego(int vidas) {
-        this.vidas = vidas;
-    }
+
+        public void MuestraVidasRestantes(){
+            System.out.println("Númoero de vidas restantes: "+vidas);
+
+        }
+
+        public boolean QuitaVida(){
+            this.vidas--;
+            if (this.vidas==0){
+                System.out.println("Juego terminado");
+                return false;
+            }else {
+                return true;
+            }
+        }
+
+        public void ReiniciaPartida(){
+            this.vidas=this.vidasIniciadas;
+        }
+
+        public void ActualizarRecord(){
+            if (this.vidas>Juego.record){
+                Juego.record=this.vidas;
+                System.out.println("Se ha batido el record: "+Juego.record);
+            }else if (this.vidas==Juego.record){
+                System.out.println("Se ha alcanzado el record");
+            }
+        }
+
+
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Juego juego1=new Juego(5);
+        Juego juego2=new Juego(5);
+        juego1.QuitaVida();
+        juego1.MuestraVidasRestantes();
+        juego1.ReiniciaPartida();
+        juego1.MuestraVidasRestantes();
+        juego1.ActualizarRecord();
+        juego2.ActualizarRecord();
 
     }
-
 }

14ef588d5a4730159d84a81e5ef7d70e6357ef50
Sandra <docenciasandrag@gmail.com>
2020-07-29 20:21:47 +0200

    JuegoAdivinaNumero y juega

---
+++ src/com/company/Aplicacion.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Aplicacion {
+
+    public static void main(String[] args) {
+        // write your code here
+        JuegoAdivinaNumero juego=new JuegoAdivinaNumero(5, 8);
+        juego.Juega();
+
+    }
+}
